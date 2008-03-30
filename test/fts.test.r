64bd6920cf63645d9f5e346757ea915ea73b19f8
Whit Armstrong <whit@lilly-armstrongs-macbook-air.local>
2008-03-30 11:18:48 -0400

    added more tests

--- test/fts.test.r
+++ test/fts.test.r
@@ -1,16 +1,30 @@
 require(fts)
 require(zoo)
 
-NC <- 10
-NR <- 10000
-WIN <- 50
+NC <- 20
+NR <- 1000
+WIN <- 20
 
 raw.data <- matrix(rnorm(NR*NC),nrow=NR,ncol=NC)
 
 x <- fts(raw.data)
 x.zoo <- zoo(raw.data, dates(x))
 
+
+## moving mean test
 fts.mean.time <- system.time(x.mean <- moving.mean(x,WIN))
 zoo.mean.time <- system.time(x.zoo.mean <- rollmean(x.zoo,WIN))
 
-print(all.equal(as.vector(x),as.vector(x.zoo)))
+stopifnot(all.equal(as.vector(x.mean[-(1:(WIN-1)),]),as.vector(x.zoo.mean)))
+
+## moving mean test
+fts.max.time <- system.time(x.max <- moving.max(x,WIN))
+zoo.max.time <- system.time(x.zoo.max <- rollmax(x.zoo,WIN))
+
+stopifnot(all.equal(as.vector(x.max[-(1:(WIN-1)),]),as.vector(x.zoo.max)))
+
+print(fts.mean.time)
+print(zoo.mean.time)
+
+print(fts.max.time)
+print(zoo.max.time)
