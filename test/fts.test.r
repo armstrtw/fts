7b4ede667e8f533ffd6c9c7bdc1e5ec3cfb07b94
whit <armstrong.whit@gmail.com>
2008-03-30 22:41:41 -0400

    updated test suite

--- test/fts.test.r
+++ test/fts.test.r
@@ -2,29 +2,29 @@ require(fts)
 require(zoo)
 
 NC <- 20
-NR <- 1000
-WIN <- 20
+NR <- 10000
+WIN <- 50
 
 raw.data <- matrix(rnorm(NR*NC),nrow=NR,ncol=NC)
 
 x <- fts(raw.data)
 x.zoo <- zoo(raw.data, dates(x))
 
+do.test <- function(test.name, fts.data, zoo.data, fts.func, zoo.func, win) {
+    cat(test.name,"\n")
 
-## moving mean test
-fts.mean.time <- system.time(x.mean <- moving.mean(x,WIN))
-zoo.mean.time <- system.time(x.zoo.mean <- rollmean(x.zoo,WIN))
+    fts.time <- system.time(fts.result <- fts.func(fts.data, win))
+    zoo.time <- system.time(zoo.result <- zoo.func(zoo.data, win))
 
-stopifnot(all.equal(as.vector(x.mean[-(1:(WIN-1)),]),as.vector(x.zoo.mean)))
+    stopifnot(all.equal(as.vector(fts.result[-(1:(win-1)),]),as.vector(zoo.result)))
 
-## moving mean test
-fts.max.time <- system.time(x.max <- moving.max(x,WIN))
-zoo.max.time <- system.time(x.zoo.max <- rollmax(x.zoo,WIN))
+    ans <- list(test=test.name,
+                fts.time=fts.time,
+                zoo.time=zoo.time)
 
-stopifnot(all.equal(as.vector(x.max[-(1:(WIN-1)),]),as.vector(x.zoo.max)))
+    ans
+}
 
-print(fts.mean.time)
-print(zoo.mean.time)
-
-print(fts.max.time)
-print(zoo.max.time)
+mean.test <- do.test("moving.mean", x, x.zoo, moving.mean, rollmean, WIN)
+max.test <- do.test("moving.max", x, x.zoo, moving.max, rollmax, WIN)
+##min.test <- do.test("moving.min", x, x.zoo, moving.min, rollmin, WIN)
